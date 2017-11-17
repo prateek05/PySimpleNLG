62f87aad7eddc33cab8dae718ae4cc47411086c1
Prateek Baranwal <prbaranw@cisco.com>
2017-11-17 13:47:20 -0800

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PySimpleNLG',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/prateek05/PySimpleNLG',
+    license='MIT',
+    author='Prateek Baranwal',
+    author_email='prateek.baranwal@gmail.com',
+    description='Python Interface to Simple NLG'
+)
